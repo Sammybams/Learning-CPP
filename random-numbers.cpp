5a0338dc8bda29c80438492806c131cfa3faa6ec
Sammybams <samuelbamgbola@gmail.com>
2023-02-24 19:17:11 +0100

    Creating truly random numbers.

--- random-numbers.cpp
+++ random-numbers.cpp
@@ -2,6 +2,22 @@
 #include <cstdlib>
 using namespace std;
 
+// pseudo random number generator with rand()
+
 int main () {
-    cout << rand() << endl;
+    srand(1);
+    /*
+    The srand() function is used to generate truly random numbers.
+    
+    This function allows to specify a seed value as its parameter,
+    which is used for the rand() function's algorithm.
+
+    Changing the seed value changes the return of rand().
+    However, the same argument will result in the same output.    
+    */
+
+    for (int x = 1; x <=10; x++) {
+        cout << rand() << endl;
+    }
+    
 }
---
+++ truly_random_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// Using current time as seed for srand()
+
+int main () {
+    srand(time(0));
+    for (int x =1; x <= 10; x++) {
+        cout << 1 + (rand() % 10) << endl;
+    }
+}
