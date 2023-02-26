f91c016a51a5b998f4b678eb888d3a259e8cf0b1
Sammybams <samuelbamgbola@gmail.com>
2023-02-26 23:54:14 +0100

    Modified.

--- truly_random_numbers.cpp
+++ truly_random_numbers.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main () {
     srand(time(0));
-    for (int x =1; x <= 10; x++) {
+    for (int x = 1; x <= 10; x++) {
         cout << 1 + (rand() % 10) << endl;
     }
 }
