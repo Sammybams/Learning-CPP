aa97f89cc909448b786726d941c870c0e89729c6
Sammybams <samuelbamgbola@gmail.com>
2023-02-27 11:22:14 +0100

    Passing arrays to functions.

--- running_tests.cpp
+++ running_tests.cpp
@@ -4,4 +4,9 @@ using namespace std;
 int main() {
     int number  = 34754;
     cout << to_string(number).length() << endl;
+
+    int arr[5]  = {3,4, 7, 5, 4};
+    cout << sizeof(arr)/sizeof(arr[0]) << endl;
+
+    return 0;
 }
