9bb3d7217c0615a91fcf34faf002622d3f9eaa30
Sammybams <samuelbamgbola@gmail.com>
2023-03-03 09:20:32 +0100

    Made changes to the original multiplicaiton table code.

--- multiplicaiton_table.cpp
+++ multiplicaiton_table.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main() {
-    cout << "Multiplication Table" << endl;
-    cout << "**************************" << endl;
-    cout << endl;
-
     int max, height;
 
     cout << "Enter horizontal range: " << endl;
@@ -18,6 +14,11 @@ int main() {
     cout << endl;
 
     int total_width = to_string(max*height).length();
+
+    cout << setw(total_width*max) <<"Multiplication Table" << endl;
+    cout << setw(total_width*max+1) << "----------------------" << endl;
+    cout << endl;
+    
     for (int i=1; i<=height; i++) {
         for (int j=1; j<=max; j++) {
             if (j==max) {
