ff4a52a793a3589fa9fbdfe81ba2e171d3c935d5
Sammybams <samuelbamgbola@gmail.com>
2023-05-26 03:55:27 +0100

    Created a script to fill arrays with random numbers.

---
+++ Test-Practice/test_practice4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void Fill_It (int TheArray[], int seed, int low, int high) {
+    
+    srand(seed);
+    for (int i = 0; i < 20; i++) {
+        TheArray[i] = low + (rand() % (high - low));
+    }
+}
+
+int main () {
+    
+    cout << "Filling Two Twenty-element arrays." << endl;
+
+    int max_length = 20;
+
+    int array1[max_length];
+    int array2[max_length];
+
+    cout << "Enter two different seed values:" << endl;
+    cout << "Seed 1: ";
+    int seed1;
+    cin >> seed1;
+    cin.ignore();
+    // cout << endl;
+
+    cout << "Seed 2: ";
+    int seed2;
+    cin >> seed2;
+    cin.ignore();
+    cout << endl;
+
+    Fill_It(array1, seed1, 1000, 2000);
+    cout << "Array 1." << endl;
+    cout << "Seed value: " << seed1 << endl;
+    cout << "[";
+    for (int i = 0; i < 19; i++) {
+        cout << array1[i] << ", ";
+    }
+    cout << array1[max_length-1] << "]" << endl;
+    cout << endl;
+
+
+    Fill_It(array2, seed2, 0, 1000);
+    cout << "Array 2." << endl;
+    cout << "Seed value: " << seed2 << endl;
+    cout << "[";
+    for (int i = 0; i < 19; i++) {
+        cout << array2[i] << ", ";
+    }
+    cout << array2[max_length-1] << "]" << endl;
+
+}
