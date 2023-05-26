94703d47c4e772f2d10cbcaca694edb9a9f7e1b9
Sammybams <samuelbamgbola@gmail.com>
2023-05-26 04:40:04 +0100

    Created banner function for display.

--- Test-Practice/test_practice5.cpp
+++ Test-Practice/test_practice5.cpp
@@ -2,3 +2,13 @@
 #include <cstdlib>
 using namespace std;
 
+void banner() {
+    cout << "Welcome to the Equation of Motion Application Programme" << endl;
+    cout << "1. First Equation: s = u*t + (1/2)*a*t^2" << endl;
+    cout << "2. Second Equation: v = u + a*t" << endl;
+    cout << "3. Third Equation: v2 = u2 + 2*a*s" << endl;
+
+    cout << endl;
+    cout << "Choice the Equation to Solve =>" << endl;
+
+}
