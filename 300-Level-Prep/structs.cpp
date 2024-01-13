7d0ea6114bf95f94d7a0d0350aa6b8236eb99691
Sammybams <samuelbamgbola@gmail.com>
2024-01-13 18:25:32 +0100

    Learning more on strucutres.

--- 300-Level-Prep/structs.cpp
+++ 300-Level-Prep/structs.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct StoreItem {
 
+    char product_name[50];
+    char UPC[20];
+    float unit_price;
+
+};
+
+int main() {
+    StoreItem StoreItem1, StoreItem2;
+
+    strcpy(StoreItem1.product_name, "Thermocool");
     cout << "Hello" << endl;
 
     return 0;
