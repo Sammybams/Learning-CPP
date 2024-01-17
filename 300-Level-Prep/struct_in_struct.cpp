b019faf7c8a481a00939c1385745d14bf6466e82
Sammybams <samuelbamgbola@gmail.com>
2024-01-17 12:32:26 +0100

    More on strucuts.

---
+++ 300-Level-Prep/struct_in_struct.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Phone {
+    int area, exch, number;
+};
+
+struct Date {
+    int month, day, year;
+};
+
+struct Employee {
+    char name[40];
+    Date hire, birth;
+    Phone home_ph;
+};
+
+int main () {
+    Employee worker;
+    strcpy(worker.name, "Hannah Marie White");
+    worker.hire.month = 1;
+    worker.hire.day =  2;
+    worker.hire.year = 1995;
+    worker.birth.month = 8;
+    worker.birth.day = 25;
+    worker.birth.year = 1972;
+    worker.home_ph.area = 505;
+    worker.home_ph.exch = 555;
+    worker.home_ph.number = 1212;
+};
+
+void groups();
+
+void groups () {
+    Employee school[1000];
+
+}
